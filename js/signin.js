719cc1c56be1d9b7e1f91cad5b9ff67f4193c042
bvmbo <wiktorsz2001@wp.pl>
2023-05-18 15:18:20 +0200

    added sending form inputs to php file when logging

--- js/signin.js
+++ js/signin.js
@@ -1,6 +1,5 @@
 const password = document.querySelector("#password");
 const email = document.querySelector("#email");
-const clearBtn = document.querySelector(".clear");
 const sendBtn = document.querySelector(".send");
 
 const showError = (input, msg) => {
@@ -16,48 +15,41 @@ const clearError = (input) => {
 	formBox.classList.remove("error");
 };
 
-const checkForm = (input) => {
-	input.forEach((el) => {
-		if (el.value === "") {
-			showError(el, el.placeholder);
-		} else {
-			clearError(el);
-		}
-	});
-};
+const checkForm = () => {
+	let formData = new FormData();
+	formData.append("haslo_logowanie", password.value);
+	formData.append("email_logowanie", email.value);
 
-const checkLength = (input, min) => {
-	if (input.value.length < min) {
-		const inputName = input.previousElementSibling.innerText;
-		showError(input, `Incerrect password`);
-	}
-};
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "../src/php/login.php");
+	xhr.send(formData);
+	xhr.onload = function () {
+		fetch("../src/php/getdata.php")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Something went wrong!");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				if (data == 0) {
+					showError(password, "Błędne hasło lub email.");
+				} else {
+					clearError(password);
+					window.location.href = "../src/panel.html";
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
-const checkMail = (input) => {
-	const re =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	if (!re.test(input.value)) {
-		showError(input, "Incorrect email.");
-	} else {
-		clearError(input);
-	}
+		return false;
+	};
 };
 
 sendBtn.addEventListener("click", (e) => {
 	e.preventDefault();
 
-	checkForm([password, email]);
-	checkLength(password, 8);
-	checkMail(email);
-});
-
-clearBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-
-	[password, email].forEach((el) => {
-		el.value = "";
-
-		clearError(el);
-	});
+	checkForm();
 });
