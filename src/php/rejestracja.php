7aa9aedfab3ee463c66e024e291745637b25a4d0
bvmbo <wiktorsz2001@wp.pl>
2023-05-18 15:16:06 +0200

    added checking if user exists in database

--- src/php/rejestracja.php
+++ src/php/rejestracja.php
@@ -1,19 +1,46 @@
 <?php
-echo "dsada";
 session_start();
 
-require("./connect.php");
+$_SESSION['pLogin1'] = False; 
+$_SESSION['pEmail1'] = False; 
 
-$login = $_POST['login_rejestracja'];
-$haslo = $_POST['haslo_rejestracja'];
-$email = $_POST['email_rejestracja'];
+require("connect.php");
+        
+    $login = $_POST['login_rejestracja'];
+    $haslo = $_POST['haslo_rejestracja'];
+    $email = $_POST['email_rejestracja'];
 
-$sql="INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `EMAIL`) VALUES (NULL, '$login', '$haslo', '$email');";
+    // //SPRAWDZANIE CZY LOGIN ISTNIEJE
+    $checkLoginCount = "SELECT LOGIN FROM users WHERE login ='$login';";
+    $check_login=$conn->query($checkLoginCount);
 
-$result= $conn->query($sql);
+    $loginRows=mysqli_num_rows($check_login);
 
 
+    //SPRAWDZANIE CZY EMAIL ISTNIEJE
+    $checkMailCount = "SELECT EMAIL FROM users WHERE email = '$email';";
+    $checkMail=$conn->query($checkMailCount);
+
+    $mailRows = mysqli_num_rows($checkMail);
+        
+    if($loginRows > 0){
+        $_SESSION['pLogin1'] = 0;
+    }
+    else{
+        $_SESSION['pLogin1'] = 1;
+    }
+
+    if($mailRows > 0){
+        $_SESSION['pEmail1'] = 0;
+    }
+    else{
+        $_SESSION['pEmail1'] = 1;
+    }
+
+    if ((isset($_SESSION['pLogin1']) && $_SESSION['pLogin1'] === 1) && (isset($_SESSION['pEmail1']) && $_SESSION['pEmail1'] === 1)){
+        $sql="INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `EMAIL`) VALUES (NULL, '$login', '$haslo', '$email');";
+        $result= $conn->query($sql);
+    }
 
 $conn->close();
-require("../po_rejestracji.html");
 ?>
