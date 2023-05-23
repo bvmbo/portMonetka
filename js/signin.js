a5b5ba9b7f741440f8a6e3f12c1f69cfde2d02d4
bvmbo <wiktorsz2001@wp.pl>
2023-05-23 22:58:21 +0200

    dodano funkcje do logowania

--- js/signin.js
+++ js/signin.js
@@ -16,36 +16,39 @@ const clearError = (input) => {
 };
 
 const checkForm = () => {
-	let formData = new FormData();
-	formData.append("haslo_logowanie", password.value);
-	formData.append("email_logowanie", email.value);
+	if (email.value != "" && password.value != "") {
+		let formData = new FormData();
+		formData.append("haslo_logowanie", password.value);
+		formData.append("email_logowanie", email.value);
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "../src/php/login.php");
-	xhr.send(formData);
-	xhr.onload = function () {
-		fetch("../src/php/getdata.php")
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Something went wrong!");
-				}
-				return response.json();
-			})
-			.then((data) => {
-				console.log(data);
-				if (data == 0) {
-					showError(password, "Błędne hasło lub email.");
-				} else {
-					clearError(password);
-					window.location.href = "../src/panel.html";
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", "../php/login.php");
+		xhr.send(formData);
+		xhr.onload = function () {
+			fetch("../php/getdata.php")
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Something went wrong!");
+					}
+					return response.json();
+				})
+				.then((data) => {
+					if (data == 0) {
+						showError(password, "Błędne hasło lub email.");
+					} else {
+						clearError(password);
+						window.location.href = "../html/panel.php";
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 
-		return false;
-	};
+			return false;
+		};
+	} else {
+		showError(password, "Błędne hasło lub email.");
+	}
 };
 
 sendBtn.addEventListener("click", (e) => {
