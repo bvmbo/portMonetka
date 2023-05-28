537f695b2ed002df142a9eabbb2962b49105a512
bvmbo <wiktorsz2001@wp.pl>
2023-05-28 19:44:24 +0200

    skrypt wyswietla formularz

--- js/panel.js
+++ js/panel.js
@@ -1,16 +1,40 @@
-const sidebar = document.querySelector(".sidebar");
-const closeBtn = document.querySelector("#btn");
-const profileName = document.querySelector(".sidebar__profile-name");
+const addTransactionPanel = document.querySelector(".add-transaction-panel");
 
-closeBtn.addEventListener("click", () => {
-	sidebar.classList.toggle("open");
-	menuBtnChange();
-});
+const nameInput = document.querySelector("#name");
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#category");
 
-const menuBtnChange = () => {
-	if (sidebar.classList.contains("open")) {
-		closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+const addTransactionBtn = document.querySelector(".add-transaction");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+
+const showPanel = () => {
+	addTransactionPanel.style.display = "flex";
+};
+
+const closePanel = () => {
+	addTransactionPanel.style.display = "none";
+	setTimeout(clearInputs, 300);
+};
+
+const checkForm = () => {
+	if (
+		nameInput.value !== "" &&
+		amountInput.value !== "" &&
+		categorySelect.value !== "none"
+	) {
+		createNewTransaction();
 	} else {
-		closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+		alert("Wypełnij wszystkie pola!");
 	}
 };
+
+const clearInputs = () => {
+	nameInput.value = "";
+	amountInput.value = "";
+	categorySelect.selectedIndex = 0;
+};
+
+addTransactionBtn.addEventListener("click", showPanel);
+cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", checkForm);
