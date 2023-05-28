5ab3bfb957716446a374b774a9508a361a19e0df
bvmbo <wiktorsz2001@wp.pl>
2023-05-28 19:41:45 +0200

    dodano wykresy

--- html/analityka.php
+++ html/analityka.php
@@ -2,6 +2,71 @@
 session_start();
 if (isset($_SESSION['czyZalogowany'])&& $_SESSION['czyZalogowany']==True)
 {
+	require("../php/connect.php");
+
+	//TUTAJ SOBIE BIORE EMAIL BO WIEM ZE JEST TYLKO JEDEN TAKI W BAZIE!
+	$email=$_SESSION['email_user'];
+	//TRANSPORT
+	$sql_transport = "SELECT Wydatki_transport FROM `users` WHERE EMAIL like '$email';";
+	$transport_result=$conn->query($sql_transport);
+	while ($row = $transport_result->fetch_assoc()) {
+		$transport=$row['Wydatki_transport'];
+	}
+	//ZAKUPY
+	$sql_zakupy = "SELECT Wydatki_zakupy FROM `users` WHERE EMAIL like '$email';";
+	$zakupy_result=$conn->query($sql_zakupy);
+	while ($row = $zakupy_result->fetch_assoc()) {
+		$zakupy=$row['Wydatki_zakupy'];
+	}
+	//ZDROWIE
+	$sql_zdrowie= "SELECT Wydatki_zdrowie FROM `users` WHERE EMAIL like '$email';";
+	$zdrowie_result=$conn->query($sql_zdrowie);
+	while ($row = $zdrowie_result->fetch_assoc()) {
+		$zdrowie=$row['Wydatki_zdrowie'];
+	}
+	//JEDZENIE
+	$sql_jedzenie= "SELECT Wydatki_jedzenie FROM `users` WHERE EMAIL like '$email';";
+	$jedzenie_result=$conn->query($sql_jedzenie);
+	while ($row = $jedzenie_result->fetch_assoc()) {
+		$jedzenie=$row['Wydatki_jedzenie'];
+	}
+	//AKTYNOŚĆ I ROZRYWKA
+	$sql_aktywnosc= "SELECT Wydatki_aktywnosc FROM `users` WHERE EMAIL like '$email';";
+	$aktywnosc_result=$conn->query($sql_aktywnosc);
+	while ($row = $aktywnosc_result->fetch_assoc()) {
+		$aktywnosc=$row['Wydatki_aktywnosc'];
+	}
+	//WYNAJEM
+	$sql_wynajem= "SELECT Wydatki_wynajem FROM `users` WHERE EMAIL like '$email';";
+	$wynajem_result=$conn->query($sql_wynajem);
+	while ($row = $wynajem_result->fetch_assoc()) {
+		$wynajem=$row['Wydatki_wynajem'];
+	}
+	//tutaj licze sobie do tego wykresu ile wydalem za wszystko 
+	$all = $transport+$zakupy+$zdrowie+$jedzenie+$aktywnosc+$wynajem;
+	//przychod
+	$sql_przychod= "SELECT przychod FROM `users` WHERE EMAIL like '$email';";
+	$przychod_result=$conn->query($sql_przychod);
+	while ($row = $przychod_result->fetch_assoc()) {
+		$przychod=$row['przychod'];
+	}
+	$_SESSION['saldo']=$przychod-$all;
+
+	$dataPoints = array(
+		array("label"=> "Jedzenie + Napoje", "y"=> $jedzenie),
+		array("label"=> "Aktywności + Rozrywka", "y"=> $aktywnosc),
+		array("label"=> "Zdrowie", "y"=> $zdrowie),
+		array("label"=> "Zakupy", "y"=> $zakupy),
+		array("label"=> "Transport", "y"=> $transport),
+		array("label"=> "Wynajem", "y"=> $wynajem),
+	);
+	$dataPoints1 = array( 
+	// array("y" => 505,"label" => "Styczeń" ),
+	// array("y" => 666,"label" => "Luty" ),
+	// array("y" => 1337,"label" => "Marzec" ),
+	// array("y" => 420,"label" => "Kwiecień" ),
+	array("y" => $all,"label" => "Maj" )
+);
 
 ?>
 <!DOCTYPE html>
@@ -30,6 +95,8 @@ if (isset($_SESSION['czyZalogowany'])&& $_SESSION['czyZalogowany']==True)
 		/>
 		<link rel="stylesheet" href="../sass/css/hamburger.css" />
 		<link rel="stylesheet" href="../sass/css/panel.css" />
+		<link rel="stylesheet" href="../sass/css/analityka.css" />
+
 	</head>
 	<body>
 		<nav class="sidebar">
@@ -75,8 +142,90 @@ if (isset($_SESSION['czyZalogowany'])&& $_SESSION['czyZalogowany']==True)
 				</li>
 			</ul>
 		</nav>
+		<script>
+			window.onload = () => {
+			
+			let chart = new CanvasJS.Chart("chartContainer", {
+				animationEnabled: true,
+				exportEnabled: true,
+				backgroundColor: "#232323",
+				fontColor: "red",
 
-		<script src="../js/panel.js"></script>
+				// title:{
+				// 	text: "Jak wydajesz swoje pieniądze?",
+				// 	fontColor: "white"
+				// },
+				legend : {
+					fontColor: "white",
+				},
+				indexlabel:{
+					fontColor: "white",
+				},
+				// subtitles: [{
+				// 	text: "Waluta: Polski złoty (zł)",
+				// 	fontColor: "white"
+				// }],
+				data: [{
+					type: "pie",
+					showInLegend: "true",
+					legendText: "{label}",
+					indexLabelFontSize: 16,
+					indexLabel: "{label} - #percent%",
+					yValueFormatString: "#,##0zł",
+					indexLabelFontColor: "white",
+
+					dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+				}]
+			});
+			chart.render();
+			
+			let chart1 = new CanvasJS.Chart("chartContainer1", {
+				animationEnabled: true,
+				backgroundColor: "#232323",
+				// title:{
+				// 	text: "W którym miesiącu wydałeś najwięcej?",
+				// 	fontColor: "white"
+				// },
+				axisY: {
+					title: "Pieniądze (w PLN)",
+					includeZero: true,
+					suffix:  "zł",
+					labelFontColor: "white",
+					fontColor:"white",
+					titleFontColor: "white",
+				},
+				legend : {
+				fontColor: "white",
+				labelFontFamily: "Raleway",
+				},
+				axisX:{
+					labelFontColor: "white",
+					labelFontSize: 20,
+					labelFontFamily: "Raleway",
+				},
+				data: [{
+					type: "bar",
+					yValueFormatString: "####,##0zł",
+					indexLabel: "{y}",
+					indexLabelPlacement: "inside",
+					indexLabelFontWeight: "bolder",
+					indexLabelFontColor: "white",
+					
+					dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+				}]
+			});
+			chart1.render();
+			}
+		</script>
+		<main class="main">
+			<h1 class="charts__title title">Jak wydajesz swoje pieniądze?</h1>
+			<div class="charts__container chart" id="chartContainer" ></div>
+			<h1 class ="charts__title title1">W którym miesiącu wydałeś najwięcej?</h1>
+			<div class="charts__container chart1" id="chartContainer1" ></div>
+		</main>
+
+		<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+		<script src="../js/sidebar.js"></script>
 	</body>
 </html>
 <?php
